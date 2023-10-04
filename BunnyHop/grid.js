6353dd1777fe95addf3728d37aafe53de51d746d
GREyklexx <GRE22432@spengergasse.at>
2023-10-04 12:07:03 +0200

    nein

--- BunnyHop/grid.js
+++ BunnyHop/grid.js
@@ -6,18 +6,21 @@ var Positiony = 0;
 var gridRow = 7;
 var l2r = true;
 function hop() {
-    if (l2r == true){
-        if (Positionx==7) {Positiony = (Positiony + 1);
+    if (l2r == true) {
+        if (Positionx == 7) {
+            Positiony = (Positiony + 1);
             bunny.style.gridRow = Positiony + 1;
             bunny.style.gridColumn = Positionx;
-            l2r=!l2r
+            l2r = !l2r
+            console.log(Positionx+Positiony+"1")
 
         }
         
-        else {Positionx = (Positionx + 1);
+        else {
+            Positionx = (Positionx + 1);
             bunny.style.gridColumn = Positionx + 1;
             bunny.style.gridRow = Positiony;
-
+            console.log(Positionx+Positiony+"2")
 
         }
     }
@@ -26,20 +29,23 @@ function hop() {
     
     else {
         
-        if (Positionx==0) {Positiony = (Positiony + 1);;
-        bunny.style.gridRow = Positiony + 1;
-        bunny.style.gridColumn = Positionx;
-        l2r=!l2r
+        if (Positionx == 0) {
+            Positiony = (Positiony + 1);;
+            bunny.style.gridRow = Positiony + 1;
+            bunny.style.gridColumn = Positionx;
+            l2r = !l2r
+            console.log(Positionx+Positiony+"3")
 
-    }
+        }
     
-        else {Positionx = (Positionx - 1);
+        else {
+            Positionx = (Positionx - 1);
             bunny.style.gridColumn = Positionx - 1;
             bunny.style.gridRow = Positiony;
+            console.log(Positionx+Positiony+"4")
         }
     }
-
 }
 
 
-var hopInterval = setInterval(hop, 100    ); 
+var hopInterval = setInterval(hop, 1000); 
